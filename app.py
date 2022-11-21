cf4f4c414949cdf2af98dc76011710bd5f4dcc20
saikrishnapuni <93872813+saikrishnapuni@users.noreply.github.com>
2022-11-21 17:36:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,6 @@ path ='train'
 known_face_encodings= []
 known_face_names = []
 mylist = os.listdir(path)
-
 for cls in mylist:
     curimg = cv2.imread(f'{path}/{cls}')
     
@@ -41,60 +40,49 @@ def makeAttendanceEntry(name):
             FILE.writelines(f'\n{name},{dtString}')
 def video(a):
    
-    capture = cv2.VideoCapture(a)
     
-    frameNr = 0
- 
-    while ( frameNr<5):
+    file_name = a
+    unknown_image = face_recognition.load_image_file(file_name)
+    unknown_image_to_draw = cv2.imread(file_name)
+
+    face_locations = face_recognition.face_locations(unknown_image)
+    face_encodings = face_recognition.face_encodings(unknown_image, face_locations)
+
+    pil_image = Image.fromarray(unknown_image)
+    draw = ImageDraw.Draw(pil_image)
+
+    for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
         
-        success, frame = capture.read()
-        
-        if success:
-            cv2.imwrite(f'test{frameNr}.jpg', frame)
- 
-        else:
-            break
- 
-        frameNr = frameNr+1
-        
- 
-    capture.release()
-    i = 0
-    c = 0
-    while(i<frameNr):
-        file_name = f"test{i}.jpg"
-        unknown_image = face_recognition.load_image_file(file_name)
-        unknown_image_to_draw = cv2.imread(file_name)
+        matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
 
-        face_locations = face_recognition.face_locations(unknown_image)
-        face_encodings = face_recognition.face_encodings(unknown_image, face_locations)
+        name = "Unknown"
 
-        pil_image = Image.fromarray(unknown_image)
-        draw = ImageDraw.Draw(pil_image)
-
-        for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
-        
-            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-
-            name = "Unknown"
-
-            face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
-            best_match_index = np.argmin(face_distances)
-        try:
-            if matches[best_match_index]:
-                name = known_face_names[best_match_index]
-                cv2.rectangle(unknown_image_to_draw,(left, top), (right, bottom), (0,255,0),3 )
-                draw.rectangle(((left, top), (right, bottom)), outline=(0, 255, 255))
-                cv2.putText(unknown_image_to_draw,name,(left,top-20), cv2.FONT_HERSHEY_SIMPLEX,1, (0,0,255), 2,cv2.LINE_AA)
-                makeAttendanceEntry(name)
-                return name
-                i+=1
+        face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+        best_match_index = np.argmin(face_distances)
+        # print(known_face_encodings)
+        # print(face_encoding)
+        print(best_match_index)
+    
+    if matches[best_match_index]:
+        name = known_face_names[best_match_index]
+        cv2.rectangle(unknown_image_to_draw,(left, top), (right, bottom), (0,255,0),3 )
+        draw.rectangle(((left, top), (right, bottom)), outline=(0, 255, 255))
+        cv2.putText(unknown_image_to_draw,name,(left,top-20), cv2.FONT_HERSHEY_SIMPLEX,1, (0,0,255), 2,cv2.LINE_AA)
+        k = ""
+        for i in name:
+            if(i.isdigit()):
+                break
+            else:
+                k = k+i
+        makeAttendanceEntry(k)
+        return name
+            
                 
                 
             
                   
-        except:
-            return "error"
+    # except:
+    #     return "error"
 app = Flask(__name__)
 @app.route('/')
 def index():
@@ -108,14 +96,7 @@ def uploader():
         f.save(os.path.join(secure_filename(f.filename)))
         a = video(f.filename)
         return render_template('done.html')
-@app.route("/Photo",methods=['GET','POST'])
-def Photo():
-    if(request.method == 'POST'):
-        f = request.files['file']
-        
-        
-        f.save(os.path.join(path,secure_filename(f.filename)))
-        return render_template('done.html')
+
 
 @app.route("/show",methods=['GET','POST'])
 def show():
